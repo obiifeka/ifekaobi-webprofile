9c66d0552782b7b5a74a139c7a14e51de40faa91
obiifeka <obiifeka5@gmail.com>
2023-06-09 12:50:23 +0100

    contact  update

---
+++ app/route.py
@@ -0,0 +1,22 @@
+from flask import redirect,url_for,render_template,request, Blueprint
+from  . import app
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # Handle POST Request here
+        return render_template('contact.html')
+    
+    # Handle GET Request here
+    return render_template('contact.html')
+
+
+ 
+  

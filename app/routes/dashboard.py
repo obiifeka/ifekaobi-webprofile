345c781def74927949eca244d7b2639b406416c5
obiifeka <obiifeka5@gmail.com>
2023-07-08 21:54:19 +0100

    APutol1

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -3,14 +3,38 @@ from flask  import  Blueprint
 
 dashboard  = Blueprint('dashboard', __name__)
 
+
 @dashboard.route('/profile/address', methods=['GET', 'POST'])
-def signup():
+def address():
     if request.method == 'POST':
    
-        return redirect(url_for('profile.dashboard'))
+        return redirect(url_for('profile/dashboard.html'))
 
     return render_template('profile/address.html')
 
+
+# Billling Address section
+
+@dashboard.route('/address/create',methods=['GET','POST'])
+def create():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('address/create.html')
+    return render_template('address/create.html')
+ 
+ # END Billling Address section
+ 
+ 
+ 
+ 
+  # PAyments  section
+
+@dashboard.route('/profile/order',methods=['GET','POST'])
+def order():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('profile/order.html')
+    return render_template('profile/order.html')
 
 
 
